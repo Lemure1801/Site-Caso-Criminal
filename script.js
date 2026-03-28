b017fff889d000796910f37d437a3a97c6515582
Lemure1801 <iangutierestf@gmail.com>
2026-03-28 00:53:24 -0300

    Testes PHP

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("O JavaScript está funcionando! O ARG já sabe que você chegou.");
+// Variáveis e tipos
+let caso = "91-1821";
+const hospital = "Santos Levíticos";
+let ano = 1991;
+let mutacaoAtiva = false;
+
+console.log(`O Caso ${caso} começou em ${ano} no ${hospital}`);
+
+// Manipulação DOM básica
+const titulo = document.querySelector('h1');
+titulo.textContent = "Caso 91-1821 – Eles Estão Olhando";
+titulo.style.color = "#8b0000"; // vermelho sangue sutil
