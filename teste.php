81cb25a81aa05267cc44a092b79ccaab3354e2bd
Lemure1801 <iangutierestf@gmail.com>
2026-06-10 14:31:59 -0300

    Teste de correção dos comentários

--- teste.php
+++
@@ -1,6 +0,0 @@
-<?php
-$path = __DIR__ . '/backend/config/config.ini';
-echo "Caminho: " . $path . "<br>";
-echo "Existe: " . (file_exists($path) ? "SIM" : "NÃO") . "<br>";
-$cfg = parse_ini_file($path, true);
-var_dump($cfg);
